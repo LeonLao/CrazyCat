1dc58fdaf739b50035c9697c435cb86b9f9cad71
LeonLao <leon_lao@126.com>
2016-02-24 18:03:31 +0800

    0224

--- app/src/main/java/com/example/crazycat/Playground.java
+++ app/src/main/java/com/example/crazycat/Playground.java
@@ -11,10 +11,17 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Toast;
 
+import java.security.KeyPairGeneratorSpi;
+import java.util.Vector;
+
 /**
  * Created by jack on 2016/2/18.
  */
 public class Playground extends SurfaceView implements View.OnTouchListener{
+
+    int k = 1;
+    //是否第一次设置路障
+    boolean justinit;
 
     private static int WIDTH = 40;
     private static final int COL = 10;
@@ -57,40 +64,73 @@ public class Playground extends SurfaceView implements View.OnTouchListener{
     private Dot getNrighbour(Dot dot,int dir){
         switch (dir){
             case 1:
-                getDot(dot.getX()-1,dot.getY());
-                break;
+                return getDot(dot.getX()-1,dot.getY());
+
             case 2:
                 if (dot.getY()%2==0){
-                    getDot(dot.getX(),dot.getY()-1);
+                    return getDot(dot.getX()-1,dot.getY()-1);
                 }else {
-                    getDot(dot.getX()-1,dot.getY()-1);
+                    return getDot(dot.getX(),dot.getY()-1);
                 }
-                break;
+
             case 3:
                 if (dot.getY()%2==0){
-                    getDot(dot.getX()+1,dot.getY()-1);
+                    return getDot(dot.getX()-1,dot.getY());
                 }else {
-                    getDot(dot.getX()-1,dot.getY());
+                    return getDot(dot.getX()+1,dot.getY()-1);
                 }
-                break;
+
             case 4:
-                getDot(dot.getX()+1,dot.getY());
-                break;
+                return getDot(dot.getX()+1,dot.getY());
+
             case 5:
                 if (dot.getY()%2==0){
-                    getDot(dot.getX(),dot.getY());
+                    return getDot(dot.getX(),dot.getY()+1);
                 }else {
-                    getDot(dot.getX(),dot.getY());
+                    return getDot(dot.getX()+1,dot.getY()+1);
                 }
-                break;
+
             case 6:
                 if (dot.getY()%2==0){
-                    getDot(dot.getX(),dot.getY()+1);
+                    return getDot(dot.getX()-1,dot.getY()+1);
                 }else {
-                    getDot(dot.getX(),dot.getY());
+                    return getDot(dot.getX(),dot.getY()+1);
                 }
+
+            default:
                 break;
         }
+        return null;
+    }
+
+    //通过返回值得到各个方向到边界的距离
+    //正数代表该方向上没路障，负值代表有路障，数字代表距离
+    private int getDistance(Dot dot,int dir){
+        int distance = 0;
+        //是否一直走到场景边缘
+        Dot ori = dot,next;
+        while (true){
+            next = getNrighbour(ori,dir);
+            //如果下一个点碰到路障
+            if (next.getStatus() == Dot.STATUS_ON){
+                return distance*-1;
+            }
+            //是否到了场景边缘
+            if (isAtEdge(next)){
+                distance++;
+                return distance;
+            }
+            distance++;
+            ori = next;
+        }
+
+    }
+
+    //猫移动的方法
+    private void MoveTo(Dot one){
+        one.setStatus(Dot.STATUS_IN);
+        getDot(cat.getX(), cat.getY()).setStatus(Dot.STATUS_OFF);
+        cat.setXY(one.getX(), one.getY());
     }
 
 
@@ -167,6 +207,7 @@ public class Playground extends SurfaceView implements View.OnTouchListener{
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
+
         if (event.getAction() == MotionEvent.ACTION_UP){
            // Toast.makeText(getContext(),event.getX()+":"+event.getY(),Toast.LENGTH_SHORT).show();
             int x,y;
@@ -177,13 +218,47 @@ public class Playground extends SurfaceView implements View.OnTouchListener{
                 x=(int)(event.getX()-WIDTH/2)/WIDTH;
             }
             if (x+1>COL||y+1>ROW){
-                initGame();
-            }else {
+               initGame();
+            }else if (getDot(x,y).getStatus() == Dot.STATUS_OFF){
+                //点可用则设置成路障
                 getDot(x,y).setStatus(Dot.STATUS_ON);
+                move();
             }
             redraw();
         }
 
         return true;
+    }
+
+    private void move() {
+        //判断是否在场景边界
+        if (isAtEdge(cat)){
+            gameOver();
+            return;
+        }
+
+        //记录器,泛型指定为Dot
+        Vector<Dot> avaliable = new Vector<>();
+        for (int i=1;i<7;i++){
+            Dot n = getNrighbour(cat,i);
+            if (n.getStatus()==Dot.STATUS_OFF){
+                //如果当前点可用，则添加进去
+                avaliable.add(n);
+            }
+        }
+        if (avaliable.size()==0){
+            win();
+        }else if (avaliable.size()==1){
+            MoveTo(avaliable.get(0));
+        }else {
+            int d = (int)(Math.random()*1000)%avaliable.size();
+            MoveTo(avaliable.get(d));
+        }
+    }
+
+    private void gameOver() {
+        Toast.makeText(getContext(),"fffff",Toast.LENGTH_SHORT).show();
+    }private void win() {
+        Toast.makeText(getContext(),"vvvvvv",Toast.LENGTH_SHORT).show();
     }
 }
